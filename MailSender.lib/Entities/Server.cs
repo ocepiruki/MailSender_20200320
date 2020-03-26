19808f3c51df5c5f8483ad4510a45f6aa0c6f8b5
Aleksandr Zaitcev <ocepiruki@gmail.com>
2020-03-26 07:37:14 +0100

    Сущности сервера и отправителя почты

---
+++ MailSender.lib/Entities/Server.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MailSender.lib.Entities
+{
+    public class Server
+    {
+        /// <summary>Почтовый сервер</summary>
+        public string Name { get; set; }
+
+        public string Address { get; set; }
+
+        public int Port { get; set; }
+
+        public bool UseSSL { get; set; }
+
+        public string Login { get; set; }
+
+        public string Password { get; set; }
+    }
+}
